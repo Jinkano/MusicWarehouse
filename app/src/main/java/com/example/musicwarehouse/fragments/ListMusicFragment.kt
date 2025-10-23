474b8b02cf3a016612ebc3faf4be760a7aeaec24
jinkis <jwir79@gmail.com>
2025-10-23 13:39:08 +0200

    WIP : We have created the adapter for the viewpager, added two fragments.

--- app/src/main/java/com/example/musicwarehouse/fragments/ListMusicFragment.kt
+++ app/src/main/java/com/example/musicwarehouse/fragments/ListMusicFragment.kt
@@ -35,7 +35,7 @@ class MusicListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music_list, container, false)
+        return inflater.inflate(R.layout.fragment_list_music, container, false)
     }
 
     companion object {
